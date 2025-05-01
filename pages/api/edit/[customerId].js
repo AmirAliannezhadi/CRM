3e1f5b52217f2d8846f2ed61dbbbb2ab261f3583
AmirAliannezhadi <amiraliannezhad.dev@gmail.com>
2025-05-01 13:11:12 +0330

    api for edit done

---
+++ pages/api/edit/[customerId].js
@@ -0,0 +1,41 @@
+import Customer from "../../../models/Customer";
+import connectDB from "../../../utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error In Connecting To DB" });
+    return;
+  }
+
+  if (req.method === "PATCH") {
+    const { customerId } = req.query;
+    const data = req.body.data;
+    try {
+      const customer = await Customer.findOne({ _id: customerId });
+      customer.name = data.name;
+      customer.lastName = data.lastName;
+      customer.email = data.email;
+      customer.phone = data.phone;
+      customer.address = data.address;
+      customer.postalCode = data.postalCode;
+      customer.date = data.date;
+      customer.products = data.products;
+      customer.updatedAt = date.now();
+      customer.save();
+      res
+        .status(201)
+        .json({ status: "Success", message: "Data Edited", data: customer });
+    } catch (error) {
+      console.log(error.message);
+      res
+        .status(500)
+        .json({ status: "failed", message: "Error In deleting Data In DB" });
+      return;
+    }
+  }
+}
