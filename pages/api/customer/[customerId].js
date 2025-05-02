2bd3d9c200f41d518e6ce1eda2db0a979c5252aa
AmirAliannezhadi <amiraliannezhad.dev@gmail.com>
2025-05-02 15:15:14 +0330

    api for get customer for edit done

---
+++ pages/api/customer/[customerId].js
@@ -0,0 +1,30 @@
+import Customer from "../../../models/Customer";
+import connectDB from "../../../utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error In Connecting To DB" });
+    return;
+  }
+
+  if (req.method === "GET") {
+    const { customerId } = req.query;
+    try {
+      const customer = Customer.findById(customerId);
+      res
+        .status(200)
+        .json({ status: "Success", message: "Data gote", data: customer });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ status: "failed", message: "Error In Storing Data In DB" });
+      return;
+    }
+  }
+}
